6f32628148a191078c4a296880948c0ab2b6346b
Fedor Serkov <serkovfb@gmail.com>
2020-04-16 10:14:11 +1000

    Класс Data считывает правильные варианты ответов.

---
+++ main.py
@@ -0,0 +1,12 @@
+class Data:
+    def __init__(self):
+        self._right_answers = [
+            line.strip().replace(' ', '') for line in open('right_answers')]
+
+    def get_right_answer(self, ticket, question):
+        return self._right_answers[ticket - 1][question - 1]
+
+
+if __name__ == '__main__':
+    data = Data()
+    print(data.get_right_answer(ticket=10, question=15))
