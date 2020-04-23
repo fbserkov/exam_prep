11a09b7616e6261ac254b61cb8b3494a2c156a57
Fedor Serkov <serkovfb@gmail.com>
2020-04-23 16:24:41 +1000

    Расчёт рейтинга "ошибочности" ответов.

--- main.py
+++ main.py
@@ -28,11 +28,25 @@ class Data:
     def get_right_answer(self, ticket, question):
         return self._db.get_result('right_answers', ticket, question)
 
-    def get_my_answer(self, ticket, question):
+    def get_my_answers(self, ticket, question):
         return self._db.get_result('my_answers', ticket, question)
+
+    def get_fallacy(self, ticket, question):
+        right_answer = self.get_right_answer(ticket, question)
+        my_answers = tuple(_[3] for _ in self.get_my_answers(ticket, question))
+        return 1 - my_answers.count(right_answer) / len(my_answers)
+
+
+def print_fallacy_rating():
+    result = tuple(
+        (i + 1, j + 1, data.get_fallacy(ticket=i+1, question=j+1))
+        for i in range(40) for j in range(20)
+    )
+    result = sorted(result, key=lambda _: _[2])
+    for ticket, question, fallacy in result:
+        print(f'{fallacy:.2f}: Билет № {ticket}, Вопрос № {question}')
 
 
 if __name__ == '__main__':
     data = Data()
-    print(data.get_right_answer(13, 16))
-    print(data.get_my_answer(13, 16))
+    print_fallacy_rating()
