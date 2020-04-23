0bef3bf9b402d5bbced82b3d8492056f244014e1
Fedor Serkov <serkovfb@gmail.com>
2020-04-23 15:34:12 +1000

    Читаю и свои ответы через отдельный класс для работы с БД.

--- main.py
+++ main.py
@@ -5,35 +5,31 @@ class Database:
     def __init__(self):
         self._right_answers = tuple(
             line.strip().replace(' ', '') for line in open('right_answers'))
+        self._my_answers = []
+        for line in open('my_answers'):
+            _date, _ticket, _question, _answer = line.split()
+            _date = None if _date == 'None' else date(
+                *map(int, _date.split('-')))
+            self._my_answers.append(
+                (_date, int(_ticket), int(_question), int(_answer)))
 
     def get_result(self, table_name, ticket, question):
         if table_name == 'right_answers':
-            return self._right_answers[ticket - 1][question - 1]
-
-
-def read_my_answers():
-    my_answers = tuple(tuple([] for _ in range(20)) for _ in range(40))
-    for line in open('my_answers'):
-        _date, _ticket, _question, _answer = line.split()
-        if _date == 'None':
-            _date = None
-        else:
-            _date = date(*map(int, _date.split('-')))
-        my_answers[int(_ticket) - 1][int(_question) - 1].append(
-            (_date, int(_answer)))
-    return my_answers
+            return int(self._right_answers[ticket - 1][question - 1])
+        if table_name == 'my_answers':
+            return tuple(_ for _ in self._my_answers if (
+                ticket, question) == (_[1], _[2]))
 
 
 class Data:
     def __init__(self):
         self._db = Database()
-        self._my_answers = read_my_answers()
 
     def get_right_answer(self, ticket, question):
         return self._db.get_result('right_answers', ticket, question)
 
     def get_my_answer(self, ticket, question):
-        return self._my_answers[ticket - 1][question - 1]
+        return self._db.get_result('my_answers', ticket, question)
 
 
 if __name__ == '__main__':
