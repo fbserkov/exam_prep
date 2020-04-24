a7c267634cbf99cfff4c3ab7ca289754607b775f
Fedor Serkov <serkovfb@gmail.com>
2020-04-24 13:44:46 +1000

    Отдельный класс для правильных ответов.

--- database.py
+++ database.py
@@ -2,23 +2,18 @@ from datetime import date
 
 
 class Database:
-    def __init__(self):
-        self._right_answers = tuple(
-            line.strip().replace(' ', '') for line in open('right_answers'))
-        self._my_answers = []
-        for line in open('my_answers'):
+    def __init__(self, filename):
+        self._data = []
+        for line in open(filename):
             _date, _ticket, _question, _answer = line.split()
             _date = None if _date == 'None' else date(
                 *map(int, _date.split('-')))
-            self._my_answers.append(
+            self._data.append(
                 (_date, int(_ticket), int(_question), int(_answer)))
 
-    def get_result(self, table_name, ticket, question):
-        if table_name == 'right_answers':
-            return int(self._right_answers[ticket - 1][question - 1])
-        if table_name == 'my_answers':
-            return tuple(_ for _ in self._my_answers if (
-                ticket, question) == (_[1], _[2]))
+    def get_value(self, ticket, question):
+        return tuple(_ for _ in self._data if (
+            ticket, question) == (_[1], _[2]))
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from database import Database
 
 
-class Data:
+class RightAnswers:
     def __init__(self):
-        self._db = Database()
+        self._right_answers = tuple(
+            line.strip().replace(' ', '') for line in open('right_answers'))
 
-    def get_right_answer(self, ticket, question):
-        return self._db.get_result('right_answers', ticket, question)
+    def get_value(self, ticket, question):
+        return int(self._right_answers[ticket - 1][question - 1])
+
+
+class MyAnswers:
+    def __init__(self):
+        self._right_answers = RightAnswers()
+        self._my_answers = Database(filename='my_answers')
 
     def get_my_answers(self, ticket, question):
-        return self._db.get_result('my_answers', ticket, question)
+        return self._my_answers.get_value(ticket, question)
 
     def get_fallacy(self, ticket, question):
-        right_answer = self.get_right_answer(ticket, question)
+        right_answer = self._right_answers.get_value(ticket, question)
         my_answers = tuple(_[3] for _ in self.get_my_answers(ticket, question))
         return 1 - my_answers.count(right_answer) / len(my_answers)
 
@@ -28,5 +35,5 @@ def print_fallacy_rating():
 
 
 if __name__ == '__main__':
-    data = Data()
+    data = MyAnswers()
     print_fallacy_rating()
