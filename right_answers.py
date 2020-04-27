164b9c533495de84beff9283e7e30087e8d3e475
Fedor Serkov <serkovfb@gmail.com>
2020-04-27 18:46:37 +1000

    Снова порешал варианты с наибольшим числом ошибок. Add some tests.

--- right_answers.py
+++ right_answers.py
@@ -1,11 +1,23 @@
+import os
+
+
 class RightAnswers:
     def __init__(self, filename):
         self._right_answers = tuple(
-            line.strip().replace(' ', '') for line in open(filename))
+            line.strip().replace(' ', '') for line in open(filename)
+            if not line.startswith('#')
+        )
 
     def get_value(self, ticket, question):
         return int(self._right_answers[ticket - 1][question - 1])
 
 
 if __name__ == '__main__':
-    pass  # for tests
+    filename = 'right_answers_test'
+    with open(filename, 'w') as file:
+        file.write('# Комментарий\n123\n246\n369\n')
+    right_answers = RightAnswers(filename)
+    assert right_answers.get_value(ticket=1, question=1) == 1
+    assert right_answers.get_value(ticket=2, question=2) == 4
+    assert right_answers.get_value(ticket=3, question=3) == 9
+    os.remove(filename)
